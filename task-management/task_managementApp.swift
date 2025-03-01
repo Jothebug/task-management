ac3cc4c8da2d257a5afd79bdd81a214aa003e91c
Jothebug <hayenbg2603@gmail.com>
2025-03-01 23:06:54 +0700

    Initial Commit

---
+++ task-management/task_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_managementApp.swift
+//  task-management
+//
+//  Created by HaYen on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct task_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
